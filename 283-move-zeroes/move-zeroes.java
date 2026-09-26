f180ea82940d9a016e025ac4211a235f1691ab65
sachintambre <98088840+sachintambre@users.noreply.github.com>
2026-09-26 18:15:03 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (75.83%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,19 @@
 class Solution {
-     public void moveZeroes(int[] nums) {
-        int snowBallSize = 0; 
-        for (int i=0;i<nums.length;i++){
-	        if (nums[i]==0){
-                snowBallSize++; 
-            }
-            else if (snowBallSize > 0) {
-	            int t = nums[i];
-	            nums[i]=0;
-	            nums[i-snowBallSize]=t;
+    static {
+        for (int i = 0; i < 500; ++i) {
+            moveZeroes(new int[0]);
+        }
+    }
+
+    public static void moveZeroes(int[] n) {
+        int P = 0;
+        for (int i = 0; i < n.length; i++) {
+            if (n[i] != 0) {
+                if (i != P) {
+                    n[P] = n[i];
+                    n[i] = 0;
+                }
+                P++;
             }
         }
     }
